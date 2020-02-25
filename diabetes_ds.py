dc4d977470f81ad410014a4afc46b085a3d9891d
Mukesh A <amukesh.mk@gmail.com>
2020-02-25 20:09:37 +0000

    added basic code for gridsearch for diabetes and iris dataset

--- diabetes_ds.py
+++ diabetes_ds.py
@@ -1,62 +1,162 @@
-import time
+import pandas as pd
 import numpy as np
-from sklearn import datasets, linear_model, svm, gaussian_process, ensemble
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.fml import FMLClient as fml
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Start Time of the execution of the program
-start_time = time.time()
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
 
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
-
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
-
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
-
-# Create regressor object
-
-# regr = linear_model.LinearRegression()
-# regr = linear_model.LogisticRegression()
-regr = linear_model.HuberRegressor()
-# regr = linear_model.Lasso()
-# regr = linear_model.Ridge()
-# regr = linear_model.BayesianRidge()
-# regr = svm.LinearSVC()
-# regr = gaussian_process.GaussianProcessRegressor()
-# regr = ensemble.AdaBoostRegressor()
-# regr = ensemble.BaggingRegressor()
-# regr = ensemble.GradientBoostingRegressor()
-# regr = ensemble.RandomForestRegressor()
+from sklearn.datasets import load_diabetes
 
 
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+# Initial model selection process
+models = []
 
-# Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('SVC', SVC(gamma='scale')))
+models.append(('LR', LogisticRegression(solver='lbfgs', max_iter=4000)))
+models.append(('DT', DecisionTreeClassifier()))
+models.append(('GNB', GaussianNB()))
+models.append(('RF', RandomForestClassifier(n_estimators=100)))
+models.append(('GB', GradientBoostingClassifier()))
 
-# The mean squared error
-mse = mean_squared_error(diabetes_y_test, diabetes_y_pred)
-print('Mean squared error: %.2f'% mse)
-# The coefficient of determination: 1 is perfect prediction
-r2 = r2_score(diabetes_y_test, diabetes_y_pred)
-print('Coefficient of determination: %.2f' % r2)
+diabetes= load_diabetes()
 
-print("--- %s seconds --- for %s" % ((time.time() - start_time), regr.__class__))
+X = diabetes.data[:, np.newaxis, 2]
+y = diabetes.target
 
-f = fml()
-# f._jprint(f.publish(regr, "MSE", mse, str(diabetes.data)))
+# Train/Test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
-# f.publish(regr, "MSE", mse, str(diabetes.data))
+names = []
+scores = []
 
-# f._jprint(f.retrieve_all_metrics(str(diabetes.data)))
+for name, model in models:
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    scores.append(accuracy_score(y_test, y_pred))
+    names.append(name)
 
-f._jprint(f.retrieve_best_metric(str(diabetes.data), True))
+tr_split = pd.DataFrame({'Name': names, 'Score': scores})
+print(tr_split)
+
+strat_k_fold = StratifiedKFold(n_splits=5, random_state=10)
+
+names = []
+scores = []
+
+for name, model in models:
+    score = cross_val_score(model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+    names.append(name)
+    scores.append(score)
+
+kf_cross_val = pd.DataFrame({'Name': names, 'Score': scores})
+print(kf_cross_val)
+
+'''
+axis = sns.barplot(x = 'Name', y = 'Score', data = kf_cross_val)
+axis.set(xlabel='Classifier', ylabel='Accuracy')
+
+for p in axis.patches:
+    height = p.get_height()
+    axis.text(p.get_x() + p.get_width()/2, height + 0.005, '{:1.4f}'.format(height), ha="center") 
+    
+plt.show()
+
+from sklearn.feature_selection import RFECV
+
+logreg_model = LogisticRegression(solver='lbfgs', max_iter=4000)
+
+rfecv = RFECV(estimator=logreg_model, step=1, cv=strat_k_fold, scoring='accuracy')
+rfecv.fit(X, y)
+
+plt.figure()
+plt.title('Logistic Regression CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+plt.show()
+
+feature_importance = list(zip(feature_names, rfecv.support_))
+
+new_features = []
+
+for key,value in enumerate(feature_importance):
+    if(value[1]) == True:
+        new_features.append(value[0])
+        
+print(new_features)
+
+# Calculate accuracy scores 
+X_new = diabetes_mod[new_features]
+
+initial_score = cross_val_score(logreg_model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Initial accuracy : {} ".format(initial_score))
+
+fe_score = cross_val_score(logreg_model, X_new, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Accuracy after Feature Selection : {} ".format(fe_score))
+
+gb_model = GradientBoostingClassifier()
+
+gb_rfecv = RFECV(estimator=gb_model, step=1, cv=strat_k_fold, scoring='accuracy')
+gb_rfecv.fit(X, y)
+
+plt.figure()
+plt.title('Gradient Boost CV score vs No of Features')
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(gb_rfecv.grid_scores_) + 1), gb_rfecv.grid_scores_)
+plt.show()
+
+feature_importance = list(zip(feature_names, gb_rfecv.support_))
+
+new_features = []
+
+for key,value in enumerate(feature_importance):
+    if(value[1]) == True:
+        new_features.append(value[0])
+        
+print(new_features)
+
+
+X_new_gb = diabetes_mod[new_features]
+
+initial_score = cross_val_score(gb_model, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Initial accuracy : {} ".format(initial_score))
+
+fe_score = cross_val_score(gb_model, X_new_gb, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Accuracy after Feature Selection : {} ".format(fe_score))
+
+from sklearn.model_selection import GridSearchCV
+
+
+# Specify parameters
+c_values = list(np.arange(1, 10))
+
+param_grid = [
+    {'C': c_values, 'penalty': ['l1'], 'solver' : ['liblinear'], 'multi_class' : ['ovr']},
+    {'C': c_values, 'penalty': ['l2'], 'solver' : ['liblinear', 'newton-cg', 'lbfgs'], 'multi_class' : ['ovr']}
+]
+
+grid = GridSearchCV(LogisticRegression(), param_grid, cv=strat_k_fold, scoring='accuracy', iid=False)
+grid.fit(X, y)
+
+
+print(grid.best_params_)
+print(grid.best_estimator_)
+
+logreg_new = LogisticRegression(C=1, multi_class='ovr', penalty='l2', solver='liblinear')
+
+initial_score = cross_val_score(logreg_new, X, y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Final accu   racy : {} ".format(initial_score))
+'''
--- iris_ds.py
+++ iris_ds.py
@@ -1,8 +1,9 @@
 import time
 import numpy as np
+import pandas as pd
 from sklearn import datasets, linear_model, svm, gaussian_process, ensemble
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, GridSearchCV
 from sklearn.fml import FMLClient as fml
 
 # Start Time of the execution of the program
@@ -15,31 +16,67 @@ Y = iris.target
 
 x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2, random_state=123)
 
-model = linear_model.LogisticRegression(C=1e5)
-# model = svm.SVC()
-# model = svm.LinearSVC()
-# model = ensemble.AdaBoostClassifier()
-# model = ensemble.BaggingClassifier()
-# model = ensemble.GradientBoostingClassifier()
-# model = ensemble.RandomForestClassifier()
-# model = gaussian_process.GaussianProcessClassifier()
+models = []
+models.append(('LR', linear_model.LogisticRegression(C=1e5)))
+models.append(('SVC', svm.SVC()))
+models.append(('LSVC',svm.LinearSVC()))
+models.append(('ABC', ensemble.AdaBoostClassifier()))
+models.append(('BC', ensemble.BaggingClassifier()))
+models.append(('GBC', ensemble.GradientBoostingClassifier()))
+models.append(('RFC', ensemble.RandomForestClassifier()))
+models.append(('GPC', gaussian_process.GaussianProcessClassifier()))
 
-# Create an instance of Logistic Regression Classifier and fit the data.
-model.fit(x_train, y_train)
+names = []
+scores = []
 
-y_pred = model.predict(x_test)
+for name, model in models:
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    scores.append(accuracy_score(y_test, y_pred))
+    names.append(name)
 
-# The mean squared error
-acc = accuracy_score(y_test, y_pred)*100
-print('Accuracy: %.2f%%'% acc)
+tr_split = pd.DataFrame({'Name': names, 'Score': scores})
+print(tr_split)
+
+strat_k_fold = StratifiedKFold(n_splits=5, random_state=10)
+
+names = []
+scores = []
+
+for name, model in models:
+    score = cross_val_score(model, X, Y, cv=strat_k_fold, scoring='accuracy').mean()
+    names.append(name)
+    scores.append(score)
+
+kf_cross_val = pd.DataFrame({'Name': names, 'Score': scores})
+print(kf_cross_val)
+
+c_values = list(np.arange(1, 10))
+
+param_grid = [
+    {'C': c_values, 'penalty': ['l1'], 'solver' : ['liblinear'], 'multi_class' : ['ovr']},
+    {'C': c_values, 'penalty': ['l2'], 'solver' : ['liblinear', 'newton-cg', 'lbfgs'], 'multi_class' : ['ovr']}
+]
+
+grid = GridSearchCV(linear_model.LogisticRegression(), param_grid, cv=strat_k_fold, scoring='accuracy', iid=False)
+grid.fit(X, Y)
+
+
+print(grid.best_params_)
+print(grid.best_estimator_)
+
+logreg_new = linear_model.LogisticRegression(C=1, multi_class='ovr', penalty='l2', solver='liblinear')
+
+initial_score = cross_val_score(logreg_new, X, Y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Final accu   racy : {} ".format(initial_score))
 
 print("--- %s seconds --- for %s" % ((time.time() - start_time), model.__class__))
 
-f = fml()
+# f = fml()
 # f._jprint(f.publish(model, "Accuracy", acc, str(iris.data)))
 
 # f.publish(model, "Accuracy", acc, str(iris.data))
 
 # f._jprint(f.retrieve_all_metrics(str(iris.data)))
 
-f._jprint(f.retrieve_best_metric(str(iris.data), False))
+# f._jprint(f.retrieve_best_metric(str(iris.data), False))
