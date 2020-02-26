9d5ae5bd2d8344d82a683cf061d7803ea493d94e
Mukesh A <amukesh.mk@gmail.com>
2020-02-26 00:48:49 +0000

    added GridSearchCV and CrossValidation for wine dataset

--- classifiers/wine_ds.py
+++ classifiers/wine_ds.py
@@ -1,45 +1,83 @@
 import time
 import numpy as np
+import pandas as pd
 from sklearn import datasets, linear_model, svm, gaussian_process, ensemble
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.fml import FMLClient as fml
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, GridSearchCV
+# from sklearn.fml import FMLClient as fml
 
 # Start Time of the execution of the program
 start_time = time.time()
 
 # import some data to play with
-wine = datasets.load_wine()
-X = wine.data
-Y = wine.target
+iris = datasets.load_wine()
+X = iris.data
+Y = iris.target
 
 x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.2, random_state=123)
 
-model = linear_model.LogisticRegression(C=1e5)
-# model = svm.SVC()
-# model = svm.LinearSVC()
-# model = ensemble.AdaBoostClassifier()
-# model = ensemble.BaggingClassifier()
-# model = ensemble.GradientBoostingClassifier()
-# model = ensemble.RandomForestClassifier()
-# model = gaussian_process.GaussianProcessClassifier()
+models = []
+models.append(('LR', linear_model.LogisticRegression(C=1e5, solver='lbfgs', max_iter=10000, multi_class='auto')))
+models.append(('SVC', svm.SVC(gamma='scale', max_iter=10000)))
+models.append(('LSVC',svm.LinearSVC(max_iter=10000)))
+models.append(('ABC', ensemble.AdaBoostClassifier()))
+models.append(('BC', ensemble.BaggingClassifier()))
+models.append(('GBC', ensemble.GradientBoostingClassifier()))
+models.append(('RFC', ensemble.RandomForestClassifier(n_estimators=10)))
+models.append(('GPC', gaussian_process.GaussianProcessClassifier()))
 
-# Create an instance of Logistic Regression Classifier and fit the data.
-model.fit(x_train, y_train)
+# finding the best model
+names = []
+scores = []
+for name, model in models:
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    scores.append(accuracy_score(y_test, y_pred))
+    names.append(name)
 
-y_pred = model.predict(x_test)
+tr_split = pd.DataFrame({'Name': names, 'Score': scores})
+print(tr_split)
 
-# The mean squared error
-acc = accuracy_score(y_test, y_pred)*100
-print('Accuracy: %.2f%%'% acc)
+# k-fold validation
+names = []
+scores = []
+strat_k_fold = StratifiedKFold(n_splits=5, random_state=10)
+
+for name, model in models:
+    score = cross_val_score(model, X, Y, cv=strat_k_fold, scoring='accuracy').mean()
+    names.append(name)
+    scores.append(score)
+
+kf_cross_val = pd.DataFrame({'Name': names, 'Score': scores})
+print(kf_cross_val)
+
+# GridSearchCV on LogisticRegression
+c_values = list(np.arange(1, 10))
+param_grid = [
+    {'C': c_values, 'penalty': ['l1'], 'solver' : ['liblinear'], 'multi_class' : ['ovr']},
+    {'C': c_values, 'penalty': ['l2'], 'solver' : ['liblinear', 'newton-cg', 'lbfgs'], 'multi_class' : ['ovr']},
+    {'C': c_values, 'penalty': ['l2'], 'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'], 'multi_class' : ['ovr']}
+]
+
+grid = GridSearchCV(linear_model.LogisticRegression(max_iter=10000), param_grid, cv=strat_k_fold, scoring='accuracy', iid=False)
+grid.fit(X, Y)
+
+print(grid.best_params_)
+print(grid.best_estimator_)
+
+# LogisticRegression on the best params
+logreg_new = linear_model.LogisticRegression(C=1, multi_class='ovr', penalty='l2', solver='liblinear')
+
+initial_score = cross_val_score(logreg_new, X, Y, cv=strat_k_fold, scoring='accuracy').mean()
+print("Final accu   racy : {} ".format(initial_score))
 
 print("--- %s seconds --- for %s" % ((time.time() - start_time), model.__class__))
 
-f = fml()
-# f._jprint(f.publish(model, "Accuracy", acc, str(wine.data)))
+# f = fml()
+# f._jprint(f.publish(model, "Accuracy", acc, str(iris.data)))
 
-# f.publish(model, "Accuracy", acc, str(wine.data))
+# f.publish(model, "Accuracy", acc, str(iris.data))
 
-# f._jprint(f.retrieve_all_metrics(str(wine.data)))
+# f._jprint(f.retrieve_all_metrics(str(iris.data)))
 
-f._jprint(f.retrieve_best_metric(str(wine.data), False))
+# f._jprint(f.retrieve_best_metric(str(iris.data), False))
